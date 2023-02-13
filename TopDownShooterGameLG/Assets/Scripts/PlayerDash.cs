4e85e06ef1571ed6da9bb7b39204f2252fb3db99
PiranhaPlant9915 <jonah@aplamon.com>
2023-02-13 12:50:17 +0100

    dash???

---
+++ TopDownShooterGameLG/Assets/Scripts/PlayerDash.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDash : MonoBehaviour
+{
+    public float dashingPower = 24f;
+    public float dashingTime = 0.2f;
+    public float dashingCooldown = 1f;
+    public bool canDash = true;
+    public bool isDashing = false;
+    public Rigidbody rigidbodyComponent;
+    BoxCollider boxCollider;
+    public KeyCode dash;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        boxCollider = gameObject.GetComponent<BoxCollider>();
+    }
+
+    // Update is called once per frame
+
+    void Update()
+    {
+        Vector2 playerMovement = new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), 0);
+        if (Input.GetKey(KeyCode.Space))
+        {
+            Debug.Log("shiz worked");
+
+        }
+    }
+    private IEnumerator Dash()
+    {
+        canDash = false;
+        isDashing = true;
+
+        //float originalGravity = rigidbodyCompone
+        yield return new WaitForSeconds(dashingCooldown);
+        isDashing = false;
+    }
+}
