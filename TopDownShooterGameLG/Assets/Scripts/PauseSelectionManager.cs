c939877d4597d1497448b141a84d49585dcd7214
PiranhaPlant9915 <jonah@aplamon.com>
2023-02-19 15:03:05 +0100

    quit to menu bug fix

--- TopDownShooterGameLG/Assets/Scripts/PauseSelectionManager.cs
+++ TopDownShooterGameLG/Assets/Scripts/PauseSelectionManager.cs
@@ -34,42 +34,48 @@ public class PauseSelectionManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Alpha3) || Input.GetKeyDown(downKey))
+        if (PauseScript.pauseEnabled)
         {
-            if (currentButtonID == mainMenuButtons.Length - 1) //if on exit, set to start
-            {
 
-                
-                currentButtonID = 0;
 
-            }
-            else
+            if (Input.GetKeyDown(KeyCode.Alpha3) || Input.GetKeyDown(downKey))
             {
-                currentButtonID += 1;
-            }
-            
-        }
-        if (Input.GetKeyDown(KeyCode.Alpha1) || Input.GetKeyDown(upKey))
-        {
-            if (currentButtonID == 0)//if on start then make to exit
-            {
-                currentButtonID = mainMenuButtons.Length - 1;
-            }
-            else
-            {
-                currentButtonID -= 1;
+                if (currentButtonID == mainMenuButtons.Length - 1) //if on exit, set to start
+                {
+
+
+                    currentButtonID = 0;
+
+                }
+                else
+                {
+                    currentButtonID += 1;
+                }
 
             }
-        }
-        currentButton = mainMenuButtons[currentButtonID];
-        Vector3 currentButtonPos = currentButton.gameObject.transform.position;
+            if (Input.GetKeyDown(KeyCode.Alpha1) || Input.GetKeyDown(upKey))
+            {
+                if (currentButtonID == 0)//if on start then make to exit
+                {
+                    currentButtonID = mainMenuButtons.Length - 1;
+                }
+                else
+                {
+                    currentButtonID -= 1;
 
-        selectionMarker.transform.position = new Vector3(selectionMarker.transform.position.x, currentButtonPos.y, currentButtonPos.z);
-            
-        if (Input.GetKeyDown(KeyCode.Alpha2) || Input.GetKeyDown(EnterKey))
-        {
-            currentButton.onClick.Invoke();
-            
+                }
+            }
+            currentButton = mainMenuButtons[currentButtonID];
+            Vector3 currentButtonPos = currentButton.gameObject.transform.position;
+
+            selectionMarker.transform.position = new Vector3(selectionMarker.transform.position.x, currentButtonPos.y, currentButtonPos.z);
+
+            if (Input.GetKeyDown(KeyCode.Alpha2) || Input.GetKeyDown(EnterKey))
+            {
+                currentButton.onClick.Invoke();
+
+            }
+
         }
     }
 }
